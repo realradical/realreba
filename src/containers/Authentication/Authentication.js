1525e698057ee36be32f165622acae4c8139ccde
realradical <jacob.z.shao@gmail.com>
2018-12-05 22:57:34 -0500

    authentication page: toggle + how it works

--- src/containers/Authentication/Authentication.js
+++ src/containers/Authentication/Authentication.js
@@ -2,33 +2,63 @@ import React, {Component} from 'react';
 
 import WithContext from "../../hoc/WithContext";
 import classes from "./Authentication.module.css";
-
 import BannerImg from "../../assets/images/test_authBanner.jpg";
-
+import HowItWorks from "../../components/HowItWorks/HowItWorks";
 
 
 class Authentication extends Component {
     state = {
-        toggleActive: "Login"
+        toggleActive: "T1"
+    };
+
+    clickHandler = (toggleType) => {
+        this.setState({toggleActive: toggleType});
     };
 
     render() {
+        let contentComponent = null;
+        let buttonCssT1 = classes["toggle-option"];
+        let buttonCssT2 = classes["toggle-option"];
+        let buttonCssT3 = classes["toggle-option"];
+
+        switch (this.state.toggleActive) {
+            case "T1":
+                buttonCssT1 = `${classes["toggle-option"]} ${classes["toggle-option-active"]}`;
+                break;
+            case "T2":
+                buttonCssT2 = `${classes["toggle-option"]} ${classes["toggle-option-active"]}`;
+                contentComponent = (<HowItWorks/>)
+                break;
+            case "T3":
+                buttonCssT3 = `${classes["toggle-option"]} ${classes["toggle-option-active"]}`;
+                break;
+            default:
+
+        }
+
         return (
             <>
                 <div className={classes.banner}>
                     {<img src={BannerImg} alt='Home Content'/>}
                 </div>
-                <div className={classes["menu-wrap"]}>
+                <div className={classes.content}>
                     <div className={classes["menu-toggle"]}>
-                        <div className={this.state.toggleActive==='Signup' ?
-                            [classes["toggle-option"],
-                                classes["toggle-option-active"]].join(" ") : classes["toggle-option"]}>
-                            Recent Work
+                        <div
+                            className={buttonCssT1}
+                            onClick={()=>this.clickHandler("T1")}
+                        >Recent Work
                         </div>
-                        <div className={this.state.toggleActive==='Login' ? [classes["toggle-option"],
-                            classes["toggle-option-active"]].join(" ") : classes["toggle-option"]}
-                        >Login</div>
+                        <div
+                            className={buttonCssT2}
+                            onClick={()=>this.clickHandler("T2")}
+                        >How It Works
+                        </div>
+                        <div
+                            className={buttonCssT3}
+                            onClick={()=>this.clickHandler("T3")}
+                        >Start Authentication</div>
                     </div>
+                    {contentComponent}
                 </div>
 
             </>
