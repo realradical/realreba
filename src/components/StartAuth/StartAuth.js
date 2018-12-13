efffc154d8e4b89d8f1eb2d7a71101d2b0ad2a8a
Jacob Shao <jacobshao@geotab.com>
2018-12-13 17:06:23 -0500

    upload images

--- src/components/StartAuth/StartAuth.js
+++ src/components/StartAuth/StartAuth.js
@@ -1,138 +1,120 @@
 import React, {Component} from 'react';
-import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
+import { Form, FormGroup, Label, Input, Col, Row } from 'reactstrap';
 import Dropzone from "react-dropzone";
+import {isMobile} from "react-device-detect";
 
-
-import { storage } from '../../firebase/firebase.js';
+import overallImg from "../../assets/images/test_overall.png";
 import classes from "./StartAuth.module.css";
 
-import Uploadedwrapper from '../imagesdashboard/imagedash.js';
 
 class StartAuth extends Component {
 
     state = {
-      image: ['http://via.placeholder.com/400x300',
-              'http://via.placeholder.com/400x300',
-              'http://via.placeholder.com/400x300',
-              'http://via.placeholder.com/400x300',
-              'http://via.placeholder.com/400x300',
-              'http://via.placeholder.com/400x300'],
-      url: '',
-      progress: 0
+        dropItems: [
+            {
+                label: 'overall',
+                placeholder: overallImg
+            },
+            {
+                label: 'itemlabel',
+                placeholder: overallImg
+            },
+            {
+                label: 'stitching',
+                placeholder: overallImg
+            },
+            {
+                label: 'insole',
+                placeholder: overallImg
+            },
+            {
+                label: 'boxlabel',
+                placeholder: overallImg
+            },
+            {
+                label: 'seal',
+                placeholder: overallImg
+            }
+        ]
     };
 
+    componentWillUnmount() {
+        // Make sure to revoke the data uris to avoid memory leaks
+        this.state.dropItems.forEach(file => URL.revokeObjectURL(file.placeholder));
+    }
 
-  componentDidMount(){
+    componentDidMount(){
       console.log(this.props.user);
-  };
+    };
 
-  onDropHandler = (file, rejectedFiles) => {
-      console.log(file)
-  }
+    onDropHandler = (file, rejectedFiles, label) => {
+      console.log(file);
+      console.log(rejectedFiles);
+      console.log(label);
+      let foundIndex = this.state.dropItems.findIndex(x => x.label === label);
+      const newdropItems = [...this.state.dropItems];
+      newdropItems[foundIndex] = {...newdropItems[foundIndex], placeholder: URL.createObjectURL(file[0])};
+      this.setState({dropItems:newdropItems});
+    };
 
-  // handleChange = e => {
-  //   if (e.target.files[0]) {
-  //     const image = e.target.files[0];
-  //     this.setState(() => ({image}));
-  //   }
-  // }
+    render() {
+        let rowArray = this.state.dropItems.map(
+          (item, index) => {
+              return index % 6 === 0 ? this.state.dropItems.slice(index, index + 6) : null;
+          }).filter(x => x != null);
 
-
-    _onChange = (e) => {
-
-    const file    = this.refs.uploadImg.files[0]
-    const reader  = new FileReader();
-    const img1 = {...this.state.image}
-    console.log(file)
-    reader.onloadend = () => {
-        this.setState({
-            image: [reader.result, reader.result, reader.result,reader.result,reader.result]
-        })
-    }
-    if (file) {
-        reader.readAsDataURL(file);
-        this.setState({
-            image: [reader.result, reader.result, reader.result,reader.result,reader.result]
-        })
-    }
-    else {
-        this.setState({
-            image: ""
-        })
-    }
-}
-
-handleClick = (e) => {
-    this.refs.uploadImg.click();
-}
-
-  handleUpload = () => {
-      const {image} = this.state;
-      const uploadTask = storage.ref("images").child(`${image.name}`).put(image);
-      uploadTask.on('state_changed',
-      (snapshot) => {
-        // progrss function ....
-        const progress = Math.round((snapshot.bytesTransferred / snapshot.totalBytes) * 100);
-        this.setState({progress});
-      },
-      (error) => {
-           // error function ....
-        console.log(error);
-      });}
-    // () => {
-    //     // complete function ....
-    //     storage.ref('images').child(image.name).getDownloadURL().then(url => {
-    //         console.log(url);
-    //         this.setState({url});
-    //     })
-    // });
-// }
-
-  render() {
-    return (
-      <div className={classes["form-wrap"]}>
-          <Form>
-              <FormGroup>
-                  <Label for="itemName">Item Name</Label>
-                  <Input type="text" name="itemName" id="itemName" placeholder="e.g. Air Jordan 11 Concord" />
-              </FormGroup>
-              <FormGroup>
-                  <Label for="description">Description</Label>
-                  <Input type="textarea" name="description" id="description"
-                         maxLength={200}
-                         rows={3}
-                         placeholder="Anything you think is worth mentioning" />
-              </FormGroup>
-              <FormGroup>
-                  <Dropzone onDrop={this.onDropHandler}>
-                          {({getRootProps, getInputProps, isDragActive}) => {
-                              return (
-                                  <div
-                                      {...getRootProps()}
+        let dropItems = rowArray.map(
+          (item, index) => {
+              return (
+                  <Row form key={index}>
+                      {item.map((i) => (
+                          <Col sm={12} md={6} lg={6} xl={4} key={i.label}>
+                              <FormGroup>
+                                  <Dropzone onDrop={(file, rejectedFiles) => this.onDropHandler(file, rejectedFiles, i.label)}
+                                            accept="image/*"
+                                            multiple={false}
+                                            maxSize={1000000}
+                                            disableClick={false}
                                   >
-                                      <input {...getInputProps()} />
-                                      {
-                                          isDragActive ?
-                                              <p>Drop files here...</p> :
-                                              <p>Try dropping some files here, or click to select files to upload.</p>
-                                      }
-                                  </div>
-                              )
-                          }}
-                      </Dropzone>
-              </FormGroup>
-          </Form>
+                                      {({getRootProps, getInputProps,isDragActive}) => {
+                                          return (
+                                              <div {...getRootProps()}
+                                                   className={classes["dropzone-wrap"]}
+                                              >
+                                                  <input {...getInputProps()} />
+                                                  <img src={i.placeholder} alt=" "/>
+                                              </div>
+                                          )
+                                      }}
+                                  </Dropzone>
+                              </FormGroup>
+                          </Col>
+                      ))}
+                  </Row>
+              )
+          }
+        );
 
-
-
-      <br/>
-        <input type="file" ref="uploadImg" onChange={this._onChange} style={{ display: 'none' }}/>
-        {/*<button onClick={this.handleUpload} >Upload</button>*/}
-        <br/>
-        <Uploadedwrapper Click = {this.handleClick} img =  {this.state.image} />
-      </div>
-    )
-  }
+        return (
+          <div className={classes["form-wrap"]}>
+              <Form>
+                  <FormGroup>
+                      <Label for="itemName">Item Name</Label>
+                      <Input type="text" name="itemName" id="itemName" placeholder="e.g. Air Jordan 11 Concord" />
+                  </FormGroup>
+                  <FormGroup>
+                      <Label for="description">Description</Label>
+                      <Input type="textarea" name="description" id="description"
+                             maxLength={200}
+                             rows={3}
+                             placeholder="Anything you think is worth mentioning" />
+                  </FormGroup>
+                  <Label>{isMobile ? "Upload pictures" : "Drag and drop picture or click to upload"}</Label>
+                  {dropItems}
+              </Form>
+          </div>
+        )
+    }
 }
 
 export default StartAuth;
--- src/components/upload_buton/uploadbutton.js
+++ src/components/upload_buton/uploadbutton.js
@@ -2,7 +2,6 @@ import { React } from "react";
 import WithContext from '../hoc/Layout';
 
 const UploadButton = () => {
-    {this.props.context.state.currentUser}
     return (
         <div>
             <button> Upload </button>
