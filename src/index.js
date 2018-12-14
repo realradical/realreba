1cd0102f332f64f46f8b2273e799c9700593fd19
Jacob Shao <jacobshao@geotab.com>
2018-12-14 14:20:39 -0500

    fixing bootstrap, user agent css

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter} from "react-router-dom";
+
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import WebFont from 'webfontloader';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import AppProvider from "./components/AppProvider"
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <AppProvider>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </AppProvider>,
+    document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
