fa40410568fbf313ca73150e807935842ff6a2c8
realradical <jacob.z.shao@gmail.com>
2019-01-14 22:40:57 -0500

    send order to email

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,11 +3,16 @@ const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 const express = require('express');
 const cors = require('cors');
-const app = express();
+const sendgridemail = require('@sendgrid/mail');
+const appCharge = express();
+const appPushOrder = express();
 
 const allowedOrigins = ['http://localhost:3000', 'https://realreba-c557f.firebaseapp.com'];
 // TODO: Remember to set token using >> firebase functions:config:set stripe.token="SECRET_STRIPE_TOKEN_HERE"
 const stripe = require('stripe')(functions.config().stripe.token);
+// TODO: Remember to set sendgrid api key using >> firebase functions:config:set sendgrid.apikey="SECRET_STRIPE_TOKEN_HERE"
+const MY_SENDGRID_API_KEY = functions.config().sendgrid.apikey;
+sendgridemail.setApiKey(MY_SENDGRID_API_KEY);
 
 admin.firestore().settings( { timestampsInSnapshots: true });
 
@@ -22,13 +27,6 @@ async function charge(req, res) {
 
     try {
         const orderRef = await admin.firestore().collection("orders").doc();
-        // const orderRef = await admin.firestore().collection('orders').add({
-        //     uid: uid,
-        //     createdAt: admin.firestore.FieldValue.serverTimestamp(),
-        //     status: 'created'
-        // });
-        //JSH:Debug EndTime
-
         // Charge card
         stripe.charges.create({
             amount,
@@ -79,6 +77,35 @@ async function charge(req, res) {
     }
 }
 
+function pushorder(req, res) {
+    const body = req.body;
+    const orderId = body.orderId;
+    const fileDownloadURL = body.fileDownloadURL;
+
+    const fileHyperlink = fileDownloadURL.map(item => "<a href=\"" + item + "\">fileDownloadURL</a>");
+    const fileDownloadURLString = fileHyperlink.join(" <br> ");
+    console.log(fileDownloadURLString);
+
+    const msg = {
+        to: 'jacob.z.shao@gmail.com',
+        from: 'noreply@authwork.com',
+        subject: 'Payment Success - OrderID: ' + orderId,
+        html: fileDownloadURLString,
+    };
+
+    sendgridemail.send(msg).then( () => {
+        send(res, 200, {
+            message: 'Success',
+            });
+    }
+    ).catch(err => {
+        console.log("SendEmail: " + err);
+        send(res, 500, {
+            error: err.message,
+        });
+    });
+}
+
 function send(res, code, body) {
     res.status(code).json({
         statusCode: code,
@@ -87,7 +114,7 @@ function send(res, code, body) {
     });
 }
 
-app.use(cors({
+appCharge.use(cors({
     origin: function(origin, callback){
         // allow requests with no origin
         // (like mobile apps or curl requests)
@@ -101,7 +128,7 @@ app.use(cors({
     }
 }));
 
-app.post('/', async (req, res) => {
+appCharge.post('/', async (req, res) => {
 
     // Catch any unexpected errors to prevent crashing
     try {
@@ -114,4 +141,32 @@ app.post('/', async (req, res) => {
     }
 });
 
-exports.charge = functions.https.onRequest(app);
+appPushOrder.use(cors({
+    origin: function(origin, callback){
+        // allow requests with no origin
+        // (like mobile apps or curl requests)
+        if(!origin) return callback(null, true);
+        if(allowedOrigins.indexOf(origin) === -1){
+            const msg = 'The CORS policy for this site does not ' +
+                'allow access from the specified Origin.';
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    }
+}));
+
+appPushOrder.post('/', (req, res) => {
+
+    // Catch any unexpected errors to prevent crashing
+    try {
+        pushorder(req, res);
+    } catch(e) {
+        console.log(e);
+        send(res, 500, {
+            error: `The server received an unexpected error. Please try again and contact the site admin if the error persists.`,
+        });
+    }
+});
+
+exports.charge = functions.https.onRequest(appCharge);
+exports.pushorder = functions.https.onRequest(appPushOrder);
