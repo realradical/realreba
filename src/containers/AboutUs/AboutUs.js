0a4d7242f8cbcfcb78854bde5e40551344c3737a
Tomide <ayotomideadedeji@gmail.com>
2019-01-19 12:07:53 -0500

    Merge branch 'master' of https://github.com/realradical/authlab
    # Conflicts:
    #	.idea/workspace.xml

--- src/containers/AboutUs/AboutUs.js
+++ src/containers/AboutUs/AboutUs.js
@@ -1,24 +1,43 @@
 import React from 'react';
-
+// import bckimg from '../../assets/images/test_authBanner.jpg'
 import classes from "./AboutUs.module.css";
 
 
 const aboutUs = () => {
     return (
-        <div className={classes.AboutUs}>
-            <h1>About Us</h1>
+            <div className={classes.AboutUs}>
+            <div className={classes.AboutUs1}>
+                <div className={classes.AboutUs2}>
+                    <h1>About Us</h1>
             <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla velit est, mattis at pulvinar vel, varius id augue. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Phasellus nec convallis massa. Nam commodo elementum convallis. Cras ut rhoncus neque, in eleifend tellus. Aenean velit quam, volutpat sit amet auctor sed, faucibus eu lorem. Fusce pulvinar interdum metus id lobortis.
+                <br/>
 
-                Ut porta massa id ante tincidunt vulputate. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Suspendisse hendrerit eu lorem fringilla posuere. Quisque tempor, orci id tincidunt aliquet, augue libero aliquam ipsum, nec efficitur velit orci sed lacus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Vivamus vel consequat ligula. Phasellus laoreet scelerisque hendrerit. Aliquam erat volutpat. Duis in nibh nibh. Pellentesque malesuada neque vel fermentum commodo. Fusce a accumsan est, in ullamcorper enim.
+                Sneakers Sneakers Sneakers! that's all we know. We have been verifying sneakers for over 15 years and we
+                know the counterfeit market. We have a team of sneakerhead authenticators, who have verified sneakers
+                from the classic "J's 11", to the infamous "Yeezy boost 750's". We know sneakers and we know when they
+                are fake or legit.
+                <br/>
+                <br/>
 
-                Duis consequat mauris eget elit pretium iaculis. Quisque leo ante, condimentum volutpat tempor sit amet, malesuada vel justo. Nullam porta pulvinar nibh sed dapibus. Curabitur tempus, elit et laoreet pulvinar, tellus ipsum vehicula dolor, ut aliquam enim est ut lorem. Nulla in erat et sem ultricies egestas. Proin faucibus condimentum neque, vitae posuere risus laoreet non. Aliquam non ligula nunc. Vivamus elementum tellus arcu, eget gravida nisi vehicula eget. Nam gravida eros non magna tristique, nec commodo velit mattis. Suspendisse vehicula augue eu erat sagittis elementum. Curabitur blandit placerat lectus, vitae tincidunt dui lobortis sed. Sed hendrerit convallis tincidunt. Vivamus feugiat mattis quam, at consectetur nibh vestibulum nec. Vestibulum varius ligula vitae eros scelerisque feugiat. Curabitur vestibulum dictum ornare. In blandit, sapien ut convallis condimentum, tortor ligula sagittis dui, vulputate lobortis nisi felis ut massa.
+                We have seen 99% replicas and can tell they are fake - even better than the actual
+                manufacturers. Our elite team of authenticators have verified over 65000 shoes since 2004, and we are
+                the leading authentication service provider in north America.
+                <br/>
+                <br/>
 
-                In non metus eget odio aliquet auctor. Phasellus ut facilisis lectus, sit amet consequat elit. Vestibulum faucibus enim neque, et ornare ante tincidunt in. Vestibulum blandit quam at lorem blandit, in placerat mauris euismod. In nec ex ligula. Suspendisse et fermentum urna, sagittis lacinia ex. Aenean urna lectus, malesuada vel risus tempor, tincidunt tristique dolor. Maecenas in mattis sapien. Vestibulum ut vulputate augue, nec venenatis urna. Integer et interdum odio. In orci nulla, lacinia eu consectetur non, ultrices ac ex. Fusce luctus nulla non bibendum dictum. Maecenas sed iaculis massa.
+                Our authentication process generates a full report on the sneakers and we provide detailed reasons why
+                a sneaker is fake or legit. The detailed report generated on the authenticated sneaker is also hosted
+                online as a record and a link to this report can be used on other sneaker retail sites
+                (e.g. stockx, kijiji, Facebook etc..) to help boost the trust of the buyers.
+                <br/>
+                <br/>
+                <br/>
+                <br/>
 
-                Vivamus tincidunt varius mauris. Quisque mollis mauris non efficitur dictum. Sed sit amet sodales est. Sed quis tellus risus. Sed vitae auctor nunc, nec faucibus tortor. Vivamus non tincidunt lacus. Vivamus orci justo, porta id venenatis in, tincidunt sit amet lectus. Duis suscipit turpis nec rhoncus vulputate. Etiam auctor gravida magna, sit amet viverra elit venenatis aliquam. In sed fringilla nunc.
             </p>
-        </div>
+                </div>
+                </div>
+            </div>
     );
 };
 
--- src/containers/Account/Account.js
+++ src/containers/Account/Account.js
@@ -1,5 +1,4 @@
 import React from 'react';
-
 import classes from "./Account.module.css";
 import WithContext from "../../hoc/WithContext";
 
@@ -8,12 +7,57 @@ import WithContext from "../../hoc/WithContext";
 const aboutUs = () => {
 
     return (
-        <div className={classes.AboutUs}>
-            <h1>My Account</h1>
-            <p>
+    <div className={classes.Myaccount}>
+        <div className={classes.Myaccount1}>
+            <div className={classes.Myaccount2}>
+                <h1>My Account</h1>
+                <table>
 
-            </p>
+                <tr>
+                    <th>Order Name</th>
+                    <th>Order Date</th>
+                    <th>Order Result</th>
+                </tr>
+                  <tr>
+                    <td>Yeezy 350 boost</td>
+                    <td>Oct-18-2018</td>
+                    <td>Legit</td>
+                </tr><tr>
+                    <td>Yeezy 350 boost</td>
+                    <td>Oct-18-2018</td>
+                    <td>Legit</td>
+                </tr><tr>
+                    <td>Yeezy 350 boost</td>
+                    <td>Oct-18-2018</td>
+                    <td>Legit</td>
+                </tr>
+                <tr>
+                    <td>Classics Jordan 11 - blue and black color ways</td>
+                    <td>Nov -08 -2017</td>
+                    <td>Fake</td>
+                </tr>
+                <tr>
+                    <td>Yeezy 350 boost</td>
+                    <td>Oct-18-2018</td>
+                    <td>Legit</td>
+                </tr>
+                <tr>
+                    <td>Classics Jordan 11 - blue and black color ways</td>
+                    <td>Nov -08 -2017</td>
+                    <td>Fake</td>
+                </tr>
+                  <tr>
+                    <td>Classics Jordan 11 - blue and black color ways</td>
+                    <td>Nov -08 -2017</td>
+                    <td>Fake</td>
+                  </tr>
+                </table>
+                <br/>
+                <br/>
+                <br/>
+            </div>
         </div>
+    </div>
     );
 };
 
