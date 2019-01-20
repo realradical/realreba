e6149dbefc104b61345cd50b77e058c284ba4ad1
Tomide <ayotomideadedeji@gmail.com>
2019-01-20 14:22:38 -0500

    Merge branch 'master' of https://github.com/realradical/authlab
    # Conflicts:
    #	.idea/workspace.xml

--- src/containers/Account/Account.js
+++ src/containers/Account/Account.js
@@ -1,18 +1,17 @@
 import React from 'react';
 import classes from "./Account.module.css";
 import WithContext from "../../hoc/WithContext";
-
-
+import bckimg from '../../assets/images/mnz-1151552-unsplash.jpg';
 
 const aboutUs = () => {
-
     return (
-    <div className={classes.Myaccount}>
-        <div className={classes.Myaccount1}>
-            <div className={classes.Myaccount2}>
+   <div>
+       <img src = {bckimg} alt = "text" height= {"100%"} width = {"100%"}/>
+        <div className={classes.overflowTest}>
+        <div className={classes.tableblock}>
+
                 <h1>My Account</h1>
                 <table>
-
                 <tr>
                     <th>Order Name</th>
                     <th>Order Date</th>
@@ -22,11 +21,34 @@ const aboutUs = () => {
                     <td>Yeezy 350 boost</td>
                     <td>Oct-18-2018</td>
                     <td>Legit</td>
-                </tr><tr>
+                </tr>
+                <tr>
                     <td>Yeezy 350 boost</td>
                     <td>Oct-18-2018</td>
                     <td>Legit</td>
-                </tr><tr>
+                </tr>
+                <tr>
+                    <td>Yeezy 350 boost</td>
+                    <td>Oct-18-2018</td>
+                    <td>Legit</td>
+                </tr>
+
+                <tr>
+                    <td>Classics Jordan 11 - blue and black color ways</td>
+                    <td>Nov -08 -2017</td>
+                    <td>Fake</td>
+                </tr>
+                <tr>
+                    <td>Yeezy 350 boost</td>
+                    <td>Oct-18-2018</td>
+                    <td>Legit</td>
+                </tr>
+                <tr>
+                    <td>Classics Jordan 11 - blue and black color ways</td>
+                    <td>Nov -08 -2017</td>
+                    <td>Fake</td>
+                </tr>
+                <tr>
                     <td>Yeezy 350 boost</td>
                     <td>Oct-18-2018</td>
                     <td>Legit</td>
@@ -46,18 +68,81 @@ const aboutUs = () => {
                     <td>Nov -08 -2017</td>
                     <td>Fake</td>
                 </tr>
-                  <tr>
+                <tr>
+                    <td>Yeezy 350 boost</td>
+                    <td>Oct-18-2018</td>
+                    <td>Legit</td>
+                </tr>
+                <tr>
                     <td>Classics Jordan 11 - blue and black color ways</td>
                     <td>Nov -08 -2017</td>
                     <td>Fake</td>
-                  </tr>
+                </tr>
+                <tr>
+                    <td>Yeezy 350 boost</td>
+                    <td>Oct-18-2018</td>
+                    <td>Legit</td>
+                </tr>
+                <tr>
+                    <td>Classics Jordan 11 - blue and black color ways</td>
+                    <td>Nov -08 -2017</td>
+                    <td>Fake</td>
+                </tr>
+                <tr>
+                    <td>Yeezy 350 boost</td>
+                    <td>Oct-18-2018</td>
+                    <td>Legit</td>
+                </tr>
+                <tr>
+                    <td>Classics Jordan 11 - blue and black color ways</td>
+                    <td>Nov -08 -2017</td>
+                    <td>Fake</td>
+                </tr>
+                <tr>
+                    <td>Yeezy 350 boost</td>
+                    <td>Oct-18-2018</td>
+                    <td>Legit</td>
+                </tr>
+                <tr>
+                    <td>Classics Jordan 11 - blue and black color ways</td>
+                    <td>Nov -08 -2017</td>
+                    <td>Fake</td>
+                </tr>
+                <tr>
+                    <td>Yeezy 350 boost</td>
+                    <td>Oct-18-2018</td>
+                    <td>Legit</td>
+                </tr>
+                <tr>
+                    <td>Classics Jordan 11 - blue and black color ways</td>
+                    <td>Nov -08 -2017</td>
+                    <td>Fake</td>
+                </tr>
+                <tr>
+                    <td>Yeezy 350 boost</td>
+                    <td>Oct-18-2018</td>
+                    <td>Legit</td>
+                </tr>
+                <tr>
+                    <td>Classics Jordan 11 - blue and black color ways</td>
+                    <td>Nov -08 -2017</td>
+                    <td>Fake</td>
+                </tr>
+                <tr>
+                    <td>Yeezy 350 boost</td>
+                    <td>Oct-18-2018</td>
+                    <td>Legit</td>
+                </tr>
+
                 </table>
-                <br/>
-                <br/>
-                <br/>
+            <br/>
+            <br/>
+
+
             </div>
         </div>
     </div>
+
     );
 };
 
