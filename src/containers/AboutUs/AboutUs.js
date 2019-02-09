3da5440f37865b68829df9acde29fac3ed73f760
Tomide <ayotomideadedeji@gmail.com>
2019-02-09 17:34:33 -0500

    Merge branch 'master' of https://github.com/realradical/authlab
    # Conflicts:
    #	.idea/workspace.xml

--- src/containers/AboutUs/AboutUs.js
+++ src/containers/AboutUs/AboutUs.js
@@ -15,24 +15,25 @@ const aboutUs = () => {
                 Sneakers Sneakers Sneakers! that's all we know. We have been verifying sneakers for over 15 years and we
                 know the counterfeit market. We have a team of sneakerhead authenticators, who have verified sneakers
                 from the classic "J's 11", to the infamous "Yeezy boost 750's". We know sneakers and we know when they
-                are fake or legit.
+                are counterfeit or legit.
                 <br/>
                 <br/>
 
                 We have seen 99% replicas and can tell they are fake - even better than the actual
-                manufacturers. Our elite team of authenticators have verified over 65000 shoes since 2004, and we are
-                the leading authentication service provider in north America.
+                manufacturers. Our elite team of authenticators have verified over 65000 shoes since 2004, and we a
+                the leading authentication service provider in North America.
                 <br/>
                 <br/>
 
+
                 Our authentication process generates a full report on the sneakers and we provide detailed reasons why
-                a sneaker is fake or legit. The detailed report generated on the authenticated sneaker is also hosted
+                a sneaker is counterfeit or legit. The detailed report generated on the authenticated sneaker is also hosted
                 online as a record and a link to this report can be used on other sneaker retail sites
-                (e.g. stockx, kijiji, Facebook etc..) to help boost the trust of the buyers.
+                (e.g. Stockx, Craiglist, Facebook etc..) to help boost the trust of the buyers.
                 <br/>
                 <br/>
                 <br/>
-                <br/>
+
 
             </p>
 
