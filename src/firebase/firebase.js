96ad15105f1b8c7c4f2dbbe83f4afb7ba18040dd
realradical <jacob.z.shao@gmail.com>
2019-02-12 20:50:38 -0500

    code optimization

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,4 +1,7 @@
-import * as firebase from 'firebase';
+import * as firebase from 'firebase/app';
+import 'firebase/auth'
+import 'firebase/storage'
+import 'firebase/firestore'
 import { devConfig } from './config';
 
 !firebase.apps.length && firebase.initializeApp(devConfig);
