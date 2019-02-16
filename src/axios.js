142e417dafad4f20434da5f6af8e713926e450ab
realradical <jacob.z.shao@gmail.com>
2019-02-15 22:05:14 -0500

    fix gitignore bug

---
+++ src/axios.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+const instance = axios.create({
+    baseURL: `https://us-central1-realreba-c557f.cloudfunctions.net/`
+});
+
+instance.interceptors.request.use(request => {
+    console.log('Starting Request', request);
+    return request
+});
+
+instance.interceptors.response.use(response => {
+    console.log('Response:', response);
+    return response
+});
+
+export default instance;
