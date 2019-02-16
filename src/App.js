4bc2629447bbfb900941575e953c1e49c6ba85b4
realradical <jacob.z.shao@gmail.com>
2019-02-15 21:43:57 -0500

    revert faq

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Layout from './hoc/Layout/Layout';
 import HomeContent from './containers/HomeContent/HomeContent';
 import AboutUs from "./containers/AboutUs/AboutUs";
 import Login from "./containers/Login/Login";
-// import Faq from "./containers/FAQ/Faq";
+import Faq from "./containers/FAQ/Faq";
 import Account from "./containers/Account/Account";
 import Error from "./containers/ErrorPage/ErrorPage";
 import PrivateRoute from "./hoc/PrivateRoute";
@@ -33,7 +33,7 @@ class App extends Component {
         <Layout>
             <Switch>
                 <Route path="/about-us" component={AboutUs}/>
-                {/*<Route path="/Faq" component={Faq}/>*/}
+                <Route path="/Faq" component={Faq}/>
                 <Route path="/authentication"  component={Authentication}/>
                 <Route path="/login" render={() => <Login redirect/>}/>
                 <PrivateRoute path="/myaccount" component={Account}/>
